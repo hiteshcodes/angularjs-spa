a734c0bf7d4d1d08c6437990311ad5d39eeace70
hitesh bhargav <hiteshbhargav420@gmail.com>
2021-04-24 17:41:43 +0530

    footer update

--- controllers/footer.js
+++ controllers/footer.js
@@ -1,3 +1,5 @@
 myApp.controller("footerController", function ($scope) {
   $scope.currYear = new Date().getFullYear();
+  $scope.owner = "Hitesh Bhargav";
+  $scope.ownerURL = "https://github.com/hiteshbhargav";
 });
